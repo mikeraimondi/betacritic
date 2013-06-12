0be7b819d84421a043f4ad6ed0dc3c798f41ebe9
acacheung <acacheung@gmail.com>
2013-06-12 16:22:15 -0400

    recently added movies module

--- spec/features/user_sees_recent_movies_spec.rb
+++ spec/features/user_sees_recent_movies_spec.rb
@@ -2,17 +2,19 @@ require 'spec_helper'
 
 feature 'User adds a movie', %{
   As a User
-  I want to add a movie for review
-  so I can review it later
+  I want to see a list of the 3 most recently added movies
+  so I can see what's new
   } do
 
-  scenario 'User can submit only if title and year are valid' do
+  scenario 'User can add a new movie and it will be top on recently added movies list' do
     visit root_path
     click_link 'Add a new movie'
     fill_in 'Title', :with => 'The Hangover Part III'
     fill_in 'Year', :with => '2013'
     fill_in 'Contributor', :with => '1'
     click_button 'Enter'
-    page.should have_content('successfully created')
+    page.should have_content('Recently added movies The Hangover')
   end
+
+  scenario 'User can click on a recently added movie and go to its show page'
 end
