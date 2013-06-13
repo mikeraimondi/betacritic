33009cba3aaf70fafcb9e94e4315b21892d77906
Xiaoxia Zhuang <xiaoxiazhuang@gmail.com>
2013-06-13 16:30:34 -0400

    resolve conflict with desired spec file

--- spec/features/user_sees_movie_spec.rb
+++ spec/features/user_sees_movie_spec.rb
@@ -7,16 +7,6 @@ feature 'User sees a movie', %{
   so I can see the reviews for the movie
   } do
 
-  # scenario 'User can submit only if title and year are valid' do
-  #   visit root_path
-  #   click_link 'Add a new movie'
-  #   fill_in 'Title', :with => 'The Hangover Part III'
-  #   fill_in 'Year', :with => '2013'
-  #   fill_in 'Contributor', :with => '1'
-  #   click_button 'Enter'
-  #   page.should have_content('successfully created')
-  # end
-
   scenario "User can select a movie from list" do
     movie = FactoryGirl.create(:movie)
     visit root_path
