5b79f407894ceccb70d1e4542255c2f54726d88c
acacheung <acacheung@gmail.com>
2013-06-17 16:03:39 -0400

    added likes table

--- spec/features/user_likes_movie_spec.rb
+++ spec/features/user_likes_movie_spec.rb
@@ -31,7 +31,24 @@ feature 'User likes a movie', %{
     page.should_not have_content('Like movie')
   end
 
-  scenario 'Signed in user can like a movie'
+  scenario 'Signed in user can like a movie' do
+    visit root_path
+    click_link 'Add a new movie'
+    fill_in 'Title', :with => 'Mulan'
+    fill_in 'Year', :with => '1996'
+    click_button 'Enter'
+    page.should have_content('Like movie')
+  end
 
-  scenario 'Signed in user can see a list of all the movies they liked'
+  scenario 'Signed in user can see a list of all the movies they liked' do
+    visit root_path
+    click_link 'Add a new movie'
+    fill_in 'Title', :with => 'Mulan'
+    fill_in 'Year', :with => '1996'
+    click_button 'Enter'
+    click_button 'Like movie'
+    within('.liked_movies') do
+      page.should have_content('Mulan (1996)')
+    end
+  end
 end
---
+++ spec/features/user_likes_reviews_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+include Warden::Test::Helpers
+
+feature 'User likes a movie', %{
+  As a logged-in user
+  I should be able to mark a movie as one I like
+  So I can see a list of all the movies that I like
+  } do
+
+  given(:user)  { FactoryGirl.create(:user) }
+
+  background do
+    Warden.test_mode!
+    login_as(user, scope: :user)
+  end
+
+  after :each do
+    Warden.test_reset!
+  end
+
+  scenario 'Guest cannot like a review'
+  #   visit root_path
+  #   click_link 'Add a new movie'
+  #   fill_in 'Title', :with => 'Mulan'
+  #   fill_in 'Year', :with => '1996'
+  #   click_button 'Enter'
+  #   click_link 'Sign Out'
+  #   within('.recently_added_movies') do
+  #     click_link 'Mulan (1996)'
+  #   end
+  #   page.should_not have_content('Like movie')
+  # end
+
+  scenario 'Signed in user can like a movie'
+  #   visit root_path
+  #   click_link 'Add a new movie'
+  #   fill_in 'Title', :with => 'Mulan'
+  #   fill_in 'Year', :with => '1996'
+  #   click_button 'Enter'
+  #   page.should have_content('Like movie')
+  # end
+
+  scenario 'Signed in user can see a list of all the movies they liked'
+  #   visit root_path
+  #   click_link 'Add a new movie'
+  #   fill_in 'Title', :with => 'Mulan'
+  #   fill_in 'Year', :with => '1996'
+  #   click_button 'Enter'
+  #   click_button 'Like movie'
+  #   within('.liked_movies') do
+  #     page.should have_content('Mulan (1996)')
+  #   end
+  # end
+end
