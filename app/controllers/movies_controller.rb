1e14fce0b5317ddaceb6d277304ef3c4c39d1d06
acacheung <acacheung@gmail.com>
2013-06-18 14:45:49 -0400

    new hash syntax

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.find(:all, :order => 'title')
+    @movies = Movie.find(:all, order: 'title')
   end
 
   def show
@@ -15,9 +15,9 @@ class MoviesController < ApplicationController
   def create
     @movie = current_user.movies.new(params[:movie])
     if @movie.save
-      redirect_to @movie, :notice => 'Movie was successfully created'
+      redirect_to @movie, notice: 'Movie was successfully created'
     else
-      render :action => 'new'
+      render action: 'new'
     end
   end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,7 +3,7 @@ class Movie < ActiveRecord::Base
 
   validates_presence_of :title, :year, :contributor
 
-  validates :year, :numericality => { greater_than_or_equal_to: 1850, less_than_or_equal_to: 2500 }
+  validates :year, numericality: { greater_than_or_equal_to: 1850, less_than_or_equal_to: 2500 }
 
   belongs_to :contributor, class_name: 'User',
     foreign_key: 'contributor_id',
